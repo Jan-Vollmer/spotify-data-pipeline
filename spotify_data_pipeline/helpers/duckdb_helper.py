90c4adca462f9837e966694676775159670a67d6
Jan <jan.vollmer@tu-dortmund.de>
2026-02-18 15:23:37 +0100

    duckdb encription added

--- spotify_data_pipeline/helpers/duckdb_helper.py
+++ spotify_data_pipeline/helpers/duckdb_helper.py
@@ -1,11 +1,16 @@
 import duckdb
+from dotenv import load_dotenv
+import os
 from pathlib import Path
+
+load_dotenv()
+key = os.getenv("DUCKDB_KEY")
 
 class DuckDBHelper:
     def __init__(self, db_path: str = "data/warehouse.duckdb"):
-        import os
-        key = os.getenv("DUCKDB_KEY")
         Path("data").mkdir(exist_ok=True)
+        if not key:
+            raise RuntimeError("DUCKDB_KEY not set in environment")
         self.con = duckdb.connect(db_path,
                                   config={"encryption_key": key} if key else {}
                                   )
